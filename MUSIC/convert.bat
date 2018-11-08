28804b0ed763fad398c92b3c8fc363121e770cb3
mills32 <biosystemxy@gmail.com>
2018-11-08 11:28:46 +0100

    Delete convert.bat

--- MUSIC/convert.bat
+++
@@ -1,3 +0,0 @@
-for /f %%a IN ('dir /b /s "C:\Mis_Mega_Cosas\MSDOS_GE\lt_eng\music\*.dro"') do call dro2imf -in %%a -out "%%a".imf -rate 70 -type 0
-rem platfor1.imf platfor11.imf /keepall /fixdro 
-pause 
