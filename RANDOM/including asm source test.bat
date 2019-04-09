ad14a30e8b5a054c43f750048d3985a4349908db
mills32 <biosystemxy@gmail.com>
2019-04-09 11:59:08 +0200

    Update

--- RANDOM/including asm source test.bat
+++
@@ -1,8 +0,0 @@
-rem compile c files
-tcc -G -O -Ic:\tc\include -ml -c main.c n_engine.c n_gus.c
-rem compile assembly files
-tasm n_test.asm;
-rem link
-tlink c0l main.obj n_engine.obj n_gus.obj n_test.obj,play.exe,,emu mathl cl -Lc:\tc\lib
-del *.MAP
-del *.OBJ
