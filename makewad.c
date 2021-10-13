12595055ef403bef402b077878479c291e70a1f9
Phil Ashby <phil.github@ashbysoft.com>
2021-10-13 12:03:14 +0100

    MSDOS runnable makewad, MAKE rules to build WAD

--- makewad.c
+++ makewad.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <string.h>
 #include <malloc.h>
 
+// TurboC++ doesn't have stdint.h
+#ifdef __GNUC__
+#include <stdint.h>
 typedef uint32_t dword;
 typedef uint8_t byte;
+#else
+typedef unsigned long dword;
+typedef unsigned char byte;
+// we are also going to be expanding wildcards ourselves..
+#include <dirent.h>
+#define USE_EXPAND
+#endif
 
 typedef struct {
 	char ident[4];
@@ -18,20 +27,24 @@ typedef struct {
 } wad_lump_t;
 
 static void upper(char *s, int n) {
-    for (int i=0; s && s[i] && i<n; i++) {
+    int i;
+    for (i=0; s && s[i] && i<n; i++) {
         if (s[i]>='a' && s[i]<='z')
             s[i] -= ('a'-'A');
     }
 }
 
 static int dumpwad(const char *wadname, int isExt) {
+    FILE *wad;
+    wad_header_t hdr;
+    wad_lump_t *idx;
+    int i;
     fprintf(stderr, "Dumping XWAD: %s\n", wadname);
-    FILE *wad = fopen(wadname, "rb");
+    wad = fopen(wadname, "rb");
     if (!wad) {
         perror("opening WAD");
         return 1;
     }
-    wad_header_t hdr;
     if (fread(&hdr, sizeof(hdr), 1, wad)!=1) {
         perror("reading header");
         return 2;
@@ -41,31 +54,34 @@ static int dumpwad(const char *wadname, int isExt) {
         perror("seeking to index");
         return 3;
     }
-    wad_lump_t *idx = calloc(hdr.count, sizeof(wad_lump_t));
+    idx = calloc(hdr.count, sizeof(wad_lump_t));
     if (fread(idx, sizeof(wad_lump_t), hdr.count, wad)!=hdr.count) {
         perror("reading index");
         return 4;
     }
-    for (int i=0; i<hdr.count; i++) {
+    for (i=0; i<hdr.count; i++) {
         fprintf(stderr, "..%d: %.32s offset=%lu, size=%lu\n", i, idx[i].name, idx[i].offset, idx[i].size);
         if (isExt) {
+            char fout[32];
+            int c;
+            FILE *out;
+            static byte buf[8192];
+            size_t n, t;
             if (fseek(wad, idx[i].offset, SEEK_SET)<0) {
                 perror("seeking content");
                 return 5;
             }
-            char fout[32];
-            for (int c=0; c<32; c++) {
+            for (c=0; c<32; c++) {
                 fout[c] = idx[i].name[c];
                 if ('/'==fout[c]) fout[c] = '-';
             }
-            FILE *out = fopen(fout, "wb");
+            out = fopen(fout, "wb");
             if (!out) {
                 perror("opening content for write");
                 return 6;
             }
-            byte buf[8192];
-            size_t n = idx[i].size;
-            size_t t = 0;
+            n = idx[i].size;
+            t = 0;
             if (n>sizeof(buf)) n = sizeof(buf);
             while (t<idx[i].size && (n=fread(buf, 1, n, wad))>0) {
                 if (fwrite(buf, 1, n, out)!=n) {
@@ -87,12 +103,65 @@ static int dumpwad(const char *wadname, int isExt) {
     return 0;
 }
 
+#ifdef USE_EXPAND
+static void expand(int *pargc, char ***pargv) {
+    int arg, argc = *pargc;
+    char **argv = *pargv;
+    int nargc = 0;
+    char **nargv = NULL;
+    // iterate supplied args, building a new list, expanding '*' via file system entries
+    for (arg=0; arg<argc; arg++) {
+        char *pwild = strchr(argv[arg], '*');
+        if (pwild) {
+            DIR *dir;
+            struct dirent *ent;
+            // wildcard found..
+            *pwild = 0;
+            dir = opendir(argv[arg]);
+            while ((ent=readdir(dir))!=NULL) {
+                char *path;
+                // knowing that _d_reserved holds the DOS DTA, along with
+                // http://www.ctyme.com/intr/rb-2977.htm &
+                // http://www.ctyme.com/intr/rb-2803.htm,
+                // we can skip directories - phew!
+                if (dir->_d_reserved[0x15] & 0x10)
+                    continue;
+                path = malloc(strlen(argv[arg])+strlen(ent->d_name)+1);
+                strcpy(path, argv[arg]);
+                strcat(path, ent->d_name);
+                if (!nargv)
+                    nargv = calloc(1, sizeof(char *));
+                else
+                    nargv = realloc(nargv, (nargc+1)*sizeof(char *));
+                nargv[nargc++] = path;
+            }
+            closedir(dir);
+        } else {
+            // plain argument
+            if (!nargv)
+                nargv = calloc(1, sizeof(char *));
+            else
+                nargv = realloc(nargv, (nargc+1)*sizeof(char *));
+            nargv[nargc++] = argv[arg];
+        }
+    }
+    if (nargv) {
+        *pargc = nargc;
+        *pargv = nargv;
+    }
+}
+#endif
+
 int main(int argc, char **argv) {
     char *wadname = "GAME.WAD";
-    int arg, isDump = 0, isExt = 0;
+    int i, arg, isDump = 0, isExt = 0;
     wad_header_t hdr;
     wad_lump_t *idx;
     FILE *wad;
+#ifdef USE_EXPAND
+    // wildcard expansion, 'cause DOS doesn't :=(
+    expand(&argc, &argv);
+#endif
     for (arg=1; arg<argc; arg++) {
         if (strncmp(argv[arg], "-w", 2)==0) {
             wadname = argv[++arg];
@@ -110,7 +179,7 @@ int main(int argc, char **argv) {
     }
     if (isDump)
         return dumpwad(wadname, isExt);
-    fprintf(stderr, "Constructing XWAD: %s\n", wadname);
+    fprintf(stdout, "Constructing XWAD: %s\n", wadname);
     wad = fopen(wadname, "wb");
     if (!wad) {
         perror("opening WAD for write");
@@ -132,9 +201,12 @@ int main(int argc, char **argv) {
         perror("allocating index memory");
         return 3;
     }
-    for (int i=0; arg<argc; arg++, i++) {
-        fprintf(stderr, "..%d: %s\n", i, argv[arg]);
-        FILE *in = fopen(argv[arg], "rb");
+    for (i=0; arg<argc; arg++, i++) {
+        FILE *in;
+        static byte buf[8192];
+        size_t n;
+        fprintf(stdout, "..%d: %s\n", i, argv[arg]);
+        in = fopen(argv[arg], "rb");
         if (!in) {
             perror("opening file for read");
             return 4;
@@ -143,8 +215,6 @@ int main(int argc, char **argv) {
         upper(idx[i].name, 32);
         idx[i].offset = hdr.offset;
         idx[i].size = 0;
-        byte buf[8192];
-        size_t n;
         while ((long)(n=fread(buf, 1, sizeof(buf), in))>0) {
             if (fwrite(buf, 1, n, wad)!=n) {
                 perror("writing file");
